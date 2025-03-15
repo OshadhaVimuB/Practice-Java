5b9af75ee97be311760169ba56b2cd7e6d3a87ed
Oshadha Vimukthi <oshadhavjb@gmail.com>
2025-03-15 11:33:33 +0530

    Implement MadLibs game using Java

---
+++ MadLibs.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class MadLibs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String adg1;
+        String noun1;
+        String adg2;
+        String verb1;
+        String adg3;
+
+        System.out.println("Enter a adjective(description):");
+        adg1 = sc.nextLine();
+        System.out.println("Enter a noun(person/animal):");
+        noun1 = sc.nextLine();
+        System.out.println("Enter a adjective(description):");
+        adg2 = sc.nextLine();
+        System.out.println("Enter a verb end with -ing(action):");
+        verb1 = sc.nextLine();
+        System.out.println("Enter a adjective(description):");
+        adg3 = sc.nextLine();
+
+
+        System.out.println("\nI went to a " + adg1 + " zoo.");
+        System.out.println("In a exhibit, I saw a " + noun1 + ".");
+        System.out.println(noun1 + " was " + adg2 + " and " + verb1 + "!");
+        System.out.println("I was " + adg3 + "!");
+    }
+}
